c31cc9969e5e7059712248529bcf7be93e9a5937
Mohamed refaat tolba tohamy ismail Elsherbini <ismail.mohamed2065@gmail.com>
2022-09-14 07:38:33 +0000

    finito

--- angRooting/src/app/app-routing.module.ts
+++ angRooting/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalsComponent } from './animals/animals.component';
 import { FruitsComponent } from './fruits/fruits.component';
+import { GenericComponent } from './generic/generic.component';
 
-const routes: Routes = [  { path: 'animals', component: AnimalsComponent},{ path: 'fruits', component: FruitsComponent}];
+const routes: Routes = [
+  {path: '', redirectTo: '/animals', pathMatch: 'full'},
+  { path: 'animals', component: AnimalsComponent},
+  { path: 'fruits', component: FruitsComponent},
+  { path: 'generic/:id', component: GenericComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
